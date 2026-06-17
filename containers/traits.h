9d4c1aaa64b5558adb17fc5986ace3ae768a2213
Aaron Davila Santos <aaron.2000.539@gmail.com>
2026-06-16 19:05:59 -0500

    chore(types): aliases Index/BucketCount/HashValue/Depth; el trait expone value_type y ref_type

--- containers/traits.h
+++ containers/traits.h
@@ -1,11 +1,13 @@
 #ifndef __TRAITS_H__
 #define __TRAITS_H__
 #include <functional> // para less y greater
+using namespace std;
 
 template <typename _Node, typename _Comp>
 struct BaseTrait{
     using Node       = _Node;
     using value_type = typename _Node::value_type;
+    using ref_type   = typename _Node::ref_type;   // el trait expone value_type y ref
     using Comp       = _Comp;
 };
 
--- types.h
+++ types.h
@@ -14,4 +14,9 @@ using T1 = int;
 
 using Ref = long;
 
+using Index       = long;   // posiciones e indices en contenedores
+using BucketCount = long;   // cantidad de buckets en HashTable
+using HashValue   = long;   // resultado de hash function
+using Depth       = long;   // profundidad/altura de arbol (AVL, BinaryTree)
+
 #endif // __TYPES_H__
