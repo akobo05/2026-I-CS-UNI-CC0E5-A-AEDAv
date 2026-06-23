8a6f48807c72ebdbfa87e16a6e9b984d60726b48
Aaron Davila Santos <aaron.2000.539@gmail.com>
2026-06-23 03:04:21 -0500

    build(btree): cablear DemoBTree en main + Makefile

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
-#include "containers/vector.h"
-#include "containers/linkedlist.h"
-// g++ -std=c++2b main.cpp containers/vector.cpp -o main
-void ListsDemo();
+// g++ -std=c++2b main.cpp containers/BTreeDemo.cpp -o main
+void DemoBTree();
 int main(){
-    // DemoVector();
-    //DemoConcurrentVector();
-    ListsDemo();
+    DemoBTree();
     return 0;
 }
