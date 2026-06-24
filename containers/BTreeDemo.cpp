b9d33e85996fb2b66dc9e8bb4d267427a15ac683
Aaron Davila Santos <aaron.2000.539@gmail.com>
2026-06-23 21:13:28 -0500

    feat(btree): adaptar demo al Trait + ForEach/FirstThat + variante descendente

--- containers/BTreeDemo.cpp
+++ containers/BTreeDemo.cpp
@@ -2,18 +2,27 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string>
+#include "../types.h"
 #include "BTree.h"
+#include "traits.h"
 
 //const char * keys="CDAMPIWNBKEHOLJYQZFXVRTSGU";
-const char * keys1 = "D1XJ2xTg8zKL9AhijOPQcEowRSp0NbW567BUfCqrs4FdtYZakHIuvGV3eMylmn";
-const char * keys2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const char * keys3 = "DYZakHIUwxVJ203ejOP9Qc8AdtuEop1XvTRghSNbW567BfiCqrs4FGMyzKLlmn";
+const TypeBTree * keys1 = "D1XJ2xTg8zKL9AhijOPQcEowRSp0NbW567BUfCqrs4FdtYZakHIuvGV3eMylmn";
+const TypeBTree * keys2 = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const TypeBTree * keys3 = "DYZakHIUwxVJ203ejOP9Qc8AdtuEop1XvTRghSNbW567BfiCqrs4FGMyzKLlmn";
 
-const int BTreeSize = 3;
-void main(int argc, char * argv[], char * envp[])
+const T1 BTreeSize = 3;
+
+void ImprimirClave(tagObjectInfo< AscBTreeTrait<TypeBTree> >& info, int nivel) { cout << info.key << " "; }
+bool EsVocal(tagObjectInfo< AscBTreeTrait<TypeBTree> >& info, int nivel) {
+    TypeBTree k = info.key;
+    return (k=='A'||k=='E'||k=='I'||k=='O'||k=='U'||k=='a'||k=='e'||k=='i'||k=='o'||k=='u');
+}
+
+void DemoBTree()
 {
        int result, i;
-       BTree <char> bt (BTreeSize);
+       BTree< AscBTreeTrait<TypeBTree> > bt (BTreeSize);
        for (i = 0; keys1[i]; i++)
        {
                //cout<<"Inserting "<<keys1[i]<<endl;
@@ -21,6 +30,25 @@ void main(int argc, char * argv[], char * envp[])
                //bt.Print(cout);
        }
        bt.Print(cout);
+
+       cout << "\nForEach:\n";
+       bt.ForEach(ImprimirClave);
+       cout << endl;
+
+       {
+               auto *pVocal = bt.FirstThat(EsVocal);
+               if( pVocal )
+                       cout << "FirstThat (vocal): " << pVocal->key << " -> " << pVocal->ObjID << endl;
+               else
+                       cout << "FirstThat (vocal): no encontrado" << endl;
+       }
+
+       // Variante descendente: el comparador viene del Trait (DescBTreeTrait)
+       cout << "\nDescendente:\n";
+       BTree< DescBTreeTrait<TypeBTree> > btDesc (BTreeSize);
+       for (i = 0; keys1[i]; i++)
+               btDesc.Insert(keys1[i], i*i);
+       btDesc.Print(cout);
        /*for (i = 0; keys2[i]; i++)
        {
                cout << "Searching " << keys2[i] << " ";
@@ -43,7 +71,7 @@ void main(int argc, char * argv[], char * envp[])
        }
        bt.Print(cout);
        cout.flush();*/
-       return 1;
+       return;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include "containers/vector.h"
-#include "containers/linkedlist.h"
+//#include "containers/vector.h"
+//#include "containers/linkedlist.h"
 // g++ -std=c++2b main.cpp containers/vector.cpp -o main
-void ListsDemo();
+//void ListsDemo();
+void DemoBTree();
 int main(){
     // DemoVector();
     //DemoConcurrentVector();
-    ListsDemo();
+    DemoBTree();
     return 0;
 }
